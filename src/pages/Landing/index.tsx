67ad0adbd90a6fafb3e68471d0db52819007e50d
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-06 15:38:17 -0300

    feat: setup tabs navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ const Landing: React.FC = () => {
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -28,7 +32,9 @@ const Landing: React.FC = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
