eb9cccb907f398c8f9c1a3e8e1cc27df8367212f
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-07 12:43:35 -0300

    feat: TeacherList and favorites

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button, Image, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image 
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/liboniluciano.png'}}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Luciano Liboni</Text>
+          <Text style={styles.subject}>Química</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        {'\n'}{'\n'}
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {' '}
+          <Text style={styles.priceValue}>R$ 80,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={heartOutlineIcon}/> */}
+            <Image source={unFavoriteIcon}/>
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon}/>
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+export default TeacherItem;
